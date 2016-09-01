bc52b43547288a374ef251c89881a37284b00f1a
Alen Shaju <alen.pshaju@gmail.com>
2016-08-31 21:16:48 -0500

    I did homework 1

--- week1/day1/1.js
+++ week1/day1/1.js
@@ -1,5 +1,10 @@
 var console = require('./lib/consolelog.js');
 
 module.exports = function triangle() {
-  // write your code here
+  var x = '#';
+  var result = x;
+  for (var i = 0; i < 7; i++) {
+   console.log(result);
+   result = result + x;
+  }
 }
--- week1/day1/3.js
+++ week1/day1/3.js
@@ -1,5 +1,24 @@
 var console = require('./lib/consolelog.js');
 
 module.exports = function chessboard() {
-  // write your code here
+  var size = 8;
+for (var i = 1; i <=size; i++){
+  var n = '';
+  for (var j = 1; j <= size; j++) {
+    if (i%2 !== j%2){
+        n = n + ' ';
+      }
+    else if (i%2 === j%2){
+        n = n + '#';
+      }
+    else if (j%2) {
+        n = n + '#';
+      }
+    else {
+        n = n + ' ';
+      }
+   }
+      console.log(n)
+
+ }
 }
